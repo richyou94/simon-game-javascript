c40b175c025a09ed61cbd4a6e47271b3191fe46f
Richard You <richardyou94@gmail.com>
2023-02-10 17:30:15 -0800

    created new function of making pattern

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
-console.log('linked')
+const btnColors = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+
+
+function nextSequence() {
+    console.log('function for next sequence')
+let randomNum = Math.floor(Math.random() * 4)
+let randomColor = btnColors[randomNum];
+gamePattern.push(randomColor);
+}
+
+nextSequence();
+console.log(gamePattern)
