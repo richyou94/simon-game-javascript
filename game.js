f57c05f6168e0a990b4d524aa133a9d94ae0b8ed
Richard You <richardyou94@gmail.com>
2023-02-10 17:34:54 -0800

    trying to figure out making sounds when click the button

--- game.js
+++ game.js
@@ -2,12 +2,18 @@ const btnColors = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 
 
+
+
 function nextSequence() {
     console.log('function for next sequence')
 let randomNum = Math.floor(Math.random() * 4)
 let randomColor = btnColors[randomNum];
+const audio = new Audio(`./sounds/${randomColor}`);
+
 gamePattern.push(randomColor);
 }
 
 nextSequence();
 console.log(gamePattern)
+
+$("#blue").fadeOut(100).fadeIn(100);
