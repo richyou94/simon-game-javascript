0089c2f47ca7a9e2876b5a84c3d68e3b0c8cbe97
Richard You <richardyou94@gmail.com>
2023-02-11 16:46:05 -0800

    refactored the playSound audio function and when user clicked the btn it plays sound

--- game.js
+++ game.js
@@ -5,16 +5,20 @@ let userPattern = [];
 $(".btn").click(function () {
   const colorClicked = this.id;
   userPattern.push(colorClicked);
+  playSound(colorClicked)
   console.log(userPattern)
 });
 
+function playSound(name) {
+    let audio = new Audio(`sounds/${name}.mp3`);
+    audio.play();
+}
 function nextSequence() {
   let randomNum = Math.floor(Math.random() * 4);
   let randomColor = btnColors[randomNum];
 
   $(`#${randomColor}`).fadeOut(100).fadeIn(100);
-  var audio = new Audio(`sounds/${randomColor}.mp3`);
-  audio.play();
+  playSound(randomColor)
   gamePattern.push(randomColor);
 }
 
