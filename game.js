2a11566c82ceef1de2c3729d91a3a42ba8ba5d8d
Richard You <richardyou94@gmail.com>
2023-02-11 17:28:24 -0800

    rearrange the function positions in javascript file

--- game.js
+++ game.js
@@ -1,32 +1,33 @@
 const btnColors = ["red", "blue", "green", "yellow"];
+let level = 0;
 let gamePattern = [];
 let userPattern = [];
 
-$(".btn").click(function () {
-  const colorClicked = this.id;
-  userPattern.push(colorClicked);
-  playSound(colorClicked)
-  animatePress(colorClicked);
-});
-
 function playSound(name) {
-    let audio = new Audio(`sounds/${name}.mp3`);
-    audio.play();
+  let audio = new Audio(`sounds/${name}.mp3`);
+  audio.play();
 }
 
 function animatePress(color) {
-    $(`#${color}`).addClass('pressed');
-    setTimeout(() => {
-        $(`#${color}`).removeClass('pressed');
-    }, 100);
+  $(`#${color}`).addClass("pressed");
+  setTimeout(() => {
+    $(`#${color}`).removeClass("pressed");
+  }, 100);
 }
+
 function nextSequence() {
   let randomNum = Math.floor(Math.random() * 4);
   let randomColor = btnColors[randomNum];
 
   $(`#${randomColor}`).fadeOut(100).fadeIn(100);
-  playSound(randomColor)
+  playSound(randomColor);
   gamePattern.push(randomColor);
 }
 
-nextSequence();
+$(".btn").click(function () {
+  const colorClicked = this.id;
+  userPattern.push(colorClicked);
+  playSound(colorClicked);
+  animatePress(colorClicked);
+});
+
