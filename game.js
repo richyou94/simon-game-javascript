f27a71fa2458b5e0dc925a1a7b1b6d79db19dcc5
Richard You <richardyou94@gmail.com>
2023-02-11 18:02:46 -0800

    level increase as I get right

--- game.js
+++ game.js
@@ -1,7 +1,26 @@
 const btnColors = ["red", "blue", "green", "yellow"];
+let isStarted = false;
 let level = 0;
 let gamePattern = [];
 let userPattern = [];
+
+$(document).keypress(function () {
+  if (!isStarted) {
+    $("#level-title").text(`Level ${level}`);
+    nextSequence();
+    isStarted = true;
+  }
+});
+
+$(".btn").click(function () {
+  const colorClicked = this.id;
+  userPattern.push(colorClicked);
+
+  playSound(colorClicked);
+  animatePress(colorClicked);
+
+  checkAnswer(userPattern.length-1);
+});
 
 function playSound(name) {
   let audio = new Audio(`sounds/${name}.mp3`);
@@ -16,18 +35,29 @@ function animatePress(color) {
 }
 
 function nextSequence() {
+  userPattern = [];
+
+  level++;
+  $("#level-title").text(`Level ${level}`);
+
   let randomNum = Math.floor(Math.random() * 4);
   let randomColor = btnColors[randomNum];
+  gamePattern.push(randomColor);
 
   $(`#${randomColor}`).fadeOut(100).fadeIn(100);
   playSound(randomColor);
-  gamePattern.push(randomColor);
 }
 
-$(".btn").click(function () {
-  const colorClicked = this.id;
-  userPattern.push(colorClicked);
-  playSound(colorClicked);
-  animatePress(colorClicked);
-});
-
+function checkAnswer(currentLevel) {
+  if (userPattern[currentLevel] === gamePattern[currentLevel]) {
+    console.log("success");
+    if (userPattern.length === gamePattern.length) {
+      setTimeout(() => {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+    playSound("wrong")
+  }
+}
